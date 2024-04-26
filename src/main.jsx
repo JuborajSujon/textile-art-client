68ddfc69d7637c98744edd474c271c6e9e026ee0
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-26 11:50:54 +0600

    install tailwind, daisyui  and intregrade with project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+    </>
+  </React.StrictMode>
+);
