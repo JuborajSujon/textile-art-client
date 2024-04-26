a77f294308a7170d88e9392cb3e35ab8cd3b88dc
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-26 12:06:05 +0600

    react helmet  provider define

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-    </>
+    <HelmetProvider>
+      <>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+      </>
+    </HelmetProvider>
   </React.StrictMode>
 );
