3af11461ead0e8840902b5ae96f06650e412659d
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-27 20:18:04 +0600

    Added loading component design and styling

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="text-center  min-h-[80vh] flex justify-center items-center">
+      <span className="loading loading-bars w-32"></span>
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import useAuth from "../../customHook/useAuth";
 import { Navigate, useLocation } from "react-router-dom";
+import Loading from "../Loading/Loading";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
@@ -8,7 +9,7 @@ const PrivateRoute = ({ children }) => {
   const location = useLocation();
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <Loading></Loading>;
   }
 
   if (!user) {
