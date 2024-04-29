529f0d52f11fd1bd9e9136b66d3190f7b659142d
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-30 05:37:55 +0600

    Animated number remove on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import HeroSlider from "../components/HeroSlider/HeroSlider";
 import ListingCategories from "./../components/ListingCategories/ListingCategories";
 import CraftItem from "../components/CraftItem/CraftItem";
 import AIChat from "../components/AIChat/AIChat";
-import AnimatedNumber from "./../components/AnimatedNumber/AnimatedNumber";
 import ClientReview from "../components/ClientReview/ClientReview";
 import { useEffect } from "react";
 import FeaturedArtisanShowcase from "../components/FeaturedArtisanShowcase/FeaturedArtisanShowcase";
@@ -22,7 +21,6 @@ const Home = () => {
       <ListingCategories />
       <CraftItem />
       <FeaturedArtisanShowcase />
-      <AnimatedNumber />
       <ClientReview />
       <AIChat />
     </div>
